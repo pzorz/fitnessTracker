2ab012a7ed61e8ead557aa80829a6abab49d28a6
Peter Zorzonello <pzjr12@gmail.com>
2024-01-16 20:02:45 -0500

    refactoring the plots

--- main.py
+++ main.py
@@ -88,22 +88,29 @@ def process_body_data(bodyData):
     x = mdates.date2num(dateArray)
 
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
+    fig = plt.figure(1)
+
+    # fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
     fig.suptitle("Body Measurements")
 
-    ax1.plot(x, bodyData['waste'].values, marker='o')
-    utils.plot_trendline(ax1, x, bodyData['waste'].values)
-    ax1.set_title('waste')
-
-    ax2.plot(x, bodyData['hips'].values, marker='o')
-    utils.plot_trendline(ax2, x, bodyData['hips'].values)
-    ax2.set_title('hips')
-
-    ax3.plot(x, bodyData['chest'].values, marker='o')
-    utils.plot_trendline(ax3, x, bodyData['chest'].values)
-    ax3.set_title('chest')
-
-    utils.date_formatter(x)
+    for key in bodyData.keys():
+        if key != 'Date':
+            ax = fig.add_subplot(111)
+            ax.plot(x, bodyData[key].values, marker='o')
+            ax.set_title(key)
+    # ax1.plot(x, bodyData['waste'].values, marker='o')
+    # utils.plot_trendline(ax1, x, bodyData['waste'].values)
+    # ax1.set_title('waste')
+    #
+    # ax2.plot(x, bodyData['hips'].values, marker='o')
+    # utils.plot_trendline(ax2, x, bodyData['hips'].values)
+    # ax2.set_title('hips')
+    #
+    # ax3.plot(x, bodyData['chest'].values, marker='o')
+    # utils.plot_trendline(ax3, x, bodyData['chest'].values)
+    # ax3.set_title('chest')
+    #
+    # utils.date_formatter(x)
 
     # save and close the figure
     plt.savefig('plots/bodyData.png')
