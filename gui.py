f1522248dbb1255a5a7297ea96c0c80feba6ec1a
Peter Zorzonello <pzjr12@gmail.com>
2024-02-02 16:38:55 -0500

    FT-34 remove test code

--- gui.py
+++ gui.py
@@ -3,17 +3,6 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 from PyQt6 import uic
 import sys
 
-def window():
-    app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
-    win.setWindowTitle("Fitness Tracker")
-
-    win.show()
-    sys.exit(app.exec_())
-
-# window()
-
 Form, Window = uic.loadUiType("fit.ui")
 
 app = QApplication([])
