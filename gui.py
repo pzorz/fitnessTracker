96fb57ab5414e32ca2fbbb45071bcfdfc5e32197
Peter Zorzonello <pzjr12@gmail.com>
2024-02-03 16:13:42 -0500

    FT-37. fix comments

--- gui.py
+++ gui.py
@@ -8,11 +8,13 @@ from PyQt6.QtWidgets import QFileDialog
 # create a class that will be able to display the autogen code. We cannot edit that file so all callbacks will
 # need to be made in this class instead.
 class Fit(Ui_MainWindow):
+    #constructor
     def __init__(self, window):
         self.setupUi(window)
         # callback for when the lifts button is pressed
         self.liftsButton.clicked.connect(self.processLifts)
 
+    # this function will open a file picker, get the file and then trigger the plot generator
     def processLifts(self):
         fname = QFileDialog.getOpenFileName(None,
                                             "Pick a CSV for the Lifting Data",
