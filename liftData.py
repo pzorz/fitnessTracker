c4e6d34f8c566f88891cf8479f642cea03d978dc
Peter Zorzonello <pzjr12@gmail.com>
2024-02-23 16:59:03 -0500

    FT-56 add popups for plots if they fail

--- liftData.py
+++ liftData.py
@@ -12,87 +12,94 @@ from PyQt6.QtWidgets import QApplication, QMessageBox
 # this function will load in the CSV of lift data, process it to do data analysis on it to get
 # usable, plot-able information. It will then call a function to plot that data.
 def processWeightedLifts(fileName, progressBar):
-    data = utils.read_csv(fileName)
+    try:
+        data = utils.read_csv(fileName)
 
-    # create a list of all the different lifts being tracked
-    liftTypes = data.Lift.unique()
+        # create a list of all the different lifts being tracked
+        liftTypes = data.Lift.unique()
 
-    # create a dictionary, the keys will be the lifts. the tables will have date and volume information
-    master = {}
-    for lift in liftTypes:
-        utils.maxVolReport[lift] = 0
-        utils.mostRecentRpt[lift] = []
+        # create a dictionary, the keys will be the lifts. the tables will have date and volume information
+        master = {}
+        for lift in liftTypes:
+            utils.maxVolReport[lift] = 0
+            utils.mostRecentRpt[lift] = []
 
-        # get all the data for one kind of Lift
-        new = data.loc[data['Lift'] == lift]
+            # get all the data for one kind of Lift
+            new = data.loc[data['Lift'] == lift]
 
-        # find the latest date for this kind of lift
-        latestDate = new['Date'].values[len(new) - 1]
+            # find the latest date for this kind of lift
+            latestDate = new['Date'].values[len(new) - 1]
 
-        # create a table of dates and volume/date for this kind of lift
-        df = pd.DataFrame(columns=['Date', 'Vol'])
+            # create a table of dates and volume/date for this kind of lift
+            df = pd.DataFrame(columns=['Date', 'Vol'])
 
-        # go over each date for this lift
-        for index, row in new.iterrows():
-            date = row['Date']
-            weight = row['weight']
+            # go over each date for this lift
+            for index, row in new.iterrows():
+                date = row['Date']
+                weight = row['weight']
 
-            # if this is not a body weight exercise
-            if not math.isnan(weight):
-                weight = utils.to_lbs(weight, row['Units'])
-                reps = row['Num_Reps']
-                sets = row['Num_Sets']
+                # if this is not a body weight exercise
+                if not math.isnan(weight):
+                    weight = utils.to_lbs(weight, row['Units'])
+                    reps = row['Num_Reps']
+                    sets = row['Num_Sets']
 
-                # if we are processing the data for the most recent date then save it for a report
-                if date == latestDate:
-                    utils.mostRecentRpt[lift].append({'Weight': weight, 'Reps': reps, 'Sets': sets})
+                    # if we are processing the data for the most recent date then save it for a report
+                    if date == latestDate:
+                        utils.mostRecentRpt[lift].append({'Weight': weight, 'Reps': reps, 'Sets': sets})
 
-                # calculate the total vol for this entry
-                vol = (reps * sets * weight)
+                    # calculate the total vol for this entry
+                    vol = (reps * sets * weight)
 
-                # this is how to check if a date has multiple entries
-                if date in df.Date.unique():
-                    # this date has already been seen so find it and then update it
-                    rowIndex = df.index[df['Date'] == date]
-                    df.loc[rowIndex, 'Vol'] = df.loc[rowIndex, 'Vol'] + vol
+                    # this is how to check if a date has multiple entries
+                    if date in df.Date.unique():
+                        # this date has already been seen so find it and then update it
+                        rowIndex = df.index[df['Date'] == date]
+                        df.loc[rowIndex, 'Vol'] = df.loc[rowIndex, 'Vol'] + vol
 
-                else:
-                    # this is the first time this date has been seen so create a new entry
-                    newRow = {"Date": row['Date'], "Vol": vol}
-                    df.loc[len(df)] = newRow
-
-            else:  # this is a body weight exercise
-                reps = row['Num_Reps']
-                sets = row['Num_Sets']
-                dur = row['Duration']
-
-                # if this is the latest date for the exercise then we can record it
-                if date == latestDate:
-                    # if dur is a string then it has the seconds' keyword in it, otherwise its NaN
-                    # we want to only save duration if this is a timed exercise since we can look at the keys later to
-                    # decide how to print the report
-                    if not issubclass(type(dur), str):
-                        utils.mostRecentRpt[lift].append({'Reps': reps, 'Sets': sets})
                     else:
-                        utils.mostRecentRpt[lift].append({'Reps': reps, 'Sets': sets, 'Dur': dur})
+                        # this is the first time this date has been seen so create a new entry
+                        newRow = {"Date": row['Date'], "Vol": vol}
+                        df.loc[len(df)] = newRow
 
-        # if the data frame is not empty (ie if this is not a body weight exercise) then add the table to a dictionary
-        if not df.empty:
-            master[lift] = df
+                else:  # this is a body weight exercise
+                    reps = row['Num_Reps']
+                    sets = row['Num_Sets']
+                    dur = row['Duration']
 
-            # for the max vol we need to find the date with the most weight
-            for index, row in df.iterrows():
-                if row['Vol'] > utils.maxVolReport[lift]:
-                    utils.maxVolReport[lift] = row['Vol']
-    plot_lifts(master, progressBar)
-    progressBar.setValue(100)
-    QApplication.processEvents()
+                    # if this is the latest date for the exercise then we can record it
+                    if date == latestDate:
+                        # if dur is a string then it has the seconds' keyword in it, otherwise its NaN
+                        # we want to only save duration if this is a timed exercise since we can look at the keys later to
+                        # decide how to print the report
+                        if not issubclass(type(dur), str):
+                            utils.mostRecentRpt[lift].append({'Reps': reps, 'Sets': sets})
+                        else:
+                            utils.mostRecentRpt[lift].append({'Reps': reps, 'Sets': sets, 'Dur': dur})
+
+            # if the data frame is not empty (ie if this is not a body weight exercise) then add the table to a dictionary
+            if not df.empty:
+                master[lift] = df
+
+                # for the max vol we need to find the date with the most weight
+                for index, row in df.iterrows():
+                    if row['Vol'] > utils.maxVolReport[lift]:
+                        utils.maxVolReport[lift] = row['Vol']
+        plot_lifts(master, progressBar)
+        progressBar.setValue(100)
+        QApplication.processEvents()
+
+    except:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Icon.Critical)
+        msg.setText("Could not generate lift data plots!")
+        x = msg.exec()
 
 
 # this function takes in the data that care about and plots it
 def plot_lifts(master, progressBar):
     num = len(master.keys())
-    numPerLift = 100/num
+    numPerLift = 100 / num
     progress = 0.0
 
     for key in master.keys():
